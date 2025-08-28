6f8ae2ffe101fc0995c0488515135f36af58021d
Valtter Vihervuori <valtter.vihervuori@supercell.com>
2025-08-28 17:18:32 +0100

    Constant offset for camera position

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float orbitTimePerRotation = 10.0f;
+
+    public float constantYOffset = 0.0f;
     public GameObject focusObject;
 
     void Update()
     {
         if (focusObject != null)
         {
-            Vector3 direction = focusObject.transform.position - transform.position;
+            Vector3 offset = new(0, constantYOffset, 0);
+            Vector3 focusPoint = focusObject.transform.position + offset;
+            Vector3 direction = focusPoint - transform.position;
             Quaternion targetRotation = Quaternion.LookRotation(direction);
             transform.rotation = targetRotation;
 
             float orbitSpeed = 360.0f / orbitTimePerRotation;
-            transform.position = focusObject.transform.position + Quaternion.Euler(0, orbitSpeed * Time.deltaTime, 0) * (transform.position - focusObject.transform.position);
+            transform.position = focusPoint + Quaternion.Euler(0, orbitSpeed * Time.deltaTime, 0) * (transform.position - focusPoint);
         }
     }
 }
